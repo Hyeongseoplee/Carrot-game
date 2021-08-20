ee68c18db861c061ce2cc723673a94af18429e71
vennylee <hyungsup42134@gmail.com>
2021-08-20 17:17:05 +0900

    feat: Delete clicked carrot and pop up retry message when you click bug

--- main.js
+++ main.js
@@ -11,6 +11,8 @@ const CARROT_COUNT = 5;
 const BUG_COUNT = 5;
 const IMG_SIZE = 80;
 const SHOWING = 'showing';
+const CARROT_CLASSNAME = 'carrot';
+const BUG_CLASSNAME = 'bug';
 
 let TIME_DURATION = 5;
 
@@ -21,6 +23,21 @@ function startGame() {
     initGame();
     startTimer();
 }
+
+filed.addEventListener('click', (e) => {
+    // 현재 started는 true.
+    if(!started){ // started 가 false라면, 게임이 시작하지 않았다면
+        return // 더 이상 함수 실행을 진행하지 않고 종료한다.
+    }
+
+    const target = e.target;
+    if(target.matches(".carrot")) {
+        target.remove();
+    }else if(target.matches(".bug")) {
+        popUp.classList.add(SHOWING);
+        return
+    }
+})
 
 function initGame() {
     addItem('carrot', CARROT_COUNT, '/img/carrot.png');
@@ -33,7 +50,21 @@ function addItem(className, count, src) {
         img.setAttribute('class', className);
         img.setAttribute('src', src);
         img.setAttribute('id',`${i}`);
-        
+
+        // filed.addEventListener('click', (e) => {
+        //     const carrot = document.querySelector('.carrot');
+        //     const target = e.target;
+            
+        //     if(target.className === CARROT_CLASSNAME) {
+        //         console.dir(target) // 클릭된 요소 하나만 삭제
+        //     }else if(e.target.className === BUG_CLASSNAME){
+        //         console.log("What the..")
+        //     }else {
+        //         console.log("nothing")
+        //     }
+        // }
+        //     )
+
         filed.appendChild(img);
 
         const x1 = 0;
@@ -50,6 +81,7 @@ function addItem(className, count, src) {
         
         img.style.left=`${leftPosition}px`;
         img.style.top=`${topPosition}px`;
+        
     }
 }
 
@@ -71,7 +103,7 @@ function startTimer() {
 
 gamePlayBtn.addEventListener('click', () => {
     if(started) { // started = true라면 (게임이 시작했다면)
-        
+
     } else {
         startGame();  // started = false라면 (게임이 시작했다면)
     }
